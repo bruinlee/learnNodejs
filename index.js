f0631d2287f14f488f71b7c8bd9e6d0cc810bd99
Xiang <bylixiang@gmail.com>
2013-11-03 17:41:17 -0800

    not to do non-blocking operation this way

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 var server = require("./server");
 var router = require("./router");
+var requestHandlers = require("./requestHandlers");
 
-server.start(router.route);
+//JS objects are just collections of name/value pairs, values can be
+//strings, numbers, functions etc.
+var handle = {}
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+server.start(router.route, handle);
--- router.js
+++ router.js
@@ -1,5 +1,11 @@
-function route(pathname){
+function route(handle, pathname){
   console.log("About to route a request for " + pathname);
+  if(typeof handle[pathname] === 'function'){
+    return handle[pathname]();//like access an element of an associative array
+  }else{
+    console.log("No request handler found for " + pathname);
+    return "404 Not Found";
+  }
 }
 
 exports.route = route;
