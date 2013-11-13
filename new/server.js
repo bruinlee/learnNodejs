acd2a846dc2e011727329d633063bb16de3b091a
Xiang <bylixiang@gmail.com>
2013-11-13 01:43:57 -0800

    11/13/2013

--- new/server.js
+++ new/server.js
@@ -1,21 +1,19 @@
 var http = require("http"),
-    url  = require("url");
+    url  = require("url"),
     fs   = require("fs");
 
 http.createServer(function (request, response){
-	var get = url.parse(request.url).pathname;
-	request.on("end", function(){
-		console.log("request end");
-		fs.readFile("test.txt", "utf-8", function(error, data){
-			response.writeHead(200, {"Content-Type": "text/plain"});
-			data = parseInt(data) + 1;
-			fs.writeFile("test.txt", data);
-			response.write("This page is refreshed " + data + " times!");
-			console.log("read file");
-			response.end("Hello");
-		});
-	});
-	response.write("here is data: " + get);
-	//response.end("Hello");
-
+  var get = url.parse(request.url).pathname;
+  console.log("server started " + get);
+  request.on('end', function(){
+    console.log("request end");
+  });
+  fs.readFile("test.txt", "utf-8", function(error, data){
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    data = parseInt(data) + 1;
+    fs.writeFile("test.txt", data);
+    response.write("This page is refreshed " + data + " times!");
+    console.log("read file");
+    response.end("Hello");
+  });
 }).listen(8888);
